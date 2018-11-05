265f1ed30318191d9a051f2ec9716ab6ab7b823c
Wicked Rainman <wicked.rainman@somewhere>
2018-11-05 16:54:44 +0000

    Code correction

--- src/macros.h
+++ src/macros.h
@@ -8,4 +8,4 @@
 #define keep_if_substring(a,b) if(substring(a,b)) keep_rec(); else drop_rec()
 #define drop_if_substring(a,b) if(substring(a,b)) drop_rec()
 #define keep_if_pattern(a,b) if(pattern(a,b)) keep_rec(); else drop_rec()
-#define drop_if_pattern(a,b) if(pattern(a,b)) drop()
+#define drop_if_pattern(a,b) if(pattern(a,b)) drop_rec()
