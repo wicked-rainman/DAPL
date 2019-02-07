3fcf8e206437bf7e3a904136e38954a25c673488
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2019-02-07 10:38:17 +0000

    Add files via upload

---
+++ example_code/sshlog-input-handler.c
@@ -0,0 +1,19 @@
+#include "../src/externs.h"
+FILE *history;
+void setup() {
+	define_input_fieldnames(' ',"month day time www sshd accepted password for reason from source_ip ");
+	set_input_directory(' ',"/shared/input/security/");
+	set_output("/shared/output/security.csv");
+	history=table("/home/damian/dev/Reference/bad_ip.tbl");
+
+}
+void loop() {
+	if(match("accepted","Accepted") and match("password","password")) keep; else drop;
+	add_field("file","ssh_logs");
+	lookup(history,"source_ip","history");
+	add_asn("source_ip");
+	add_country("source_ip");
+	add_reverse_dns("source_ip");
+	add_concatenation("dtg"," day '/' month '/2018:' time");
+	write_csv_fields("file reason dtg source_ip source_ip.asn source_ip.country source_ip.rdns history");
+}
