553040548ec6e5e83a463f3151e03b20905900ff
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2019-02-17 18:23:57 +0000

    Update find_fieldname.c

--- src/find_fieldname.c
+++ src/find_fieldname.c
@@ -2,11 +2,8 @@
 int find_fieldname(char* fieldname) {
 int k=0;
 	if(_drop==1) return(-1);
-
-	//while(k<_field_count) {
-        //        if(strcmp(_fieldnames_array[k],fieldname)==0) return(k);
-        //        k++;
-        //}
-	for(k=0;k<_field_count;k++) if(strcmp(_fieldnames_array[k],fieldname)==0) return(k);
+	for(k=0;k<_field_count;k++) {
+		if(strcmp(_fieldnames_array[k],fieldname)==0) return(k);
+	}
         return(-1);
 }
