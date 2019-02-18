66361d5447c595fb844388612af786cdace57f32
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2019-02-18 08:07:57 +0000

    Update remove_string.c

--- src/remove_string.c
+++ src/remove_string.c
@@ -1,13 +1,12 @@
 #include "externs.h"
 void remove_string(char *fieldname, char *substring) {
 int k,l;
-size_t substring_length,end_pos,start_pos,mainstring_length;
+size_t end_pos,start_pos,mainstring_length;
 char newstring[MAX_FIELDVALUE_LENGTH];
 	if(_drop==1) return;
 	k=find_fieldname(fieldname);
 	if(k>=0) {
 		mainstring_length=strlen(_fieldvalues_array[k]);
-		substring_length=strlen(substring);
 		l=strpos(_fieldvalues_array[k],substring);
 		if(l >= 0) {
 			start_pos= (size_t) l;
